350d8fbe6fbb5001f2d5eb29771a6d641074182c
VICTOR CAETANO <victor.c.souto@academico.unirv.edu.br>
2024-04-24 16:36:05 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class HotelModel(db.Model):
+    __tablename__ = 'hotels'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"Hotel(name = {self.name}, city = {self.city}, address = {self.address})"
+
+
+db.create_all()
+
+hotel_put_args = reqparse.RequestParser()
+hotel_put_args.add_argument("name", type=str, help="Name is required", required=True)
+hotel_put_args.add_argument("city", type=str, help="City is required", required=True)
+hotel_put_args.add_argument("address", type=str, help="Address is required", required=True)
+
+hotel_update_args = reqparse.RequestParser()
+hotel_update_args.add_argument("name", type=str, help="Name is required")
+hotel_update_args.add_argument("city", type=str, help="City is required")
+hotel_update_args.add_argument("address", type=str, help="Address is required")
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'city': fields.String,
+    'address': fields.String
+}
+
+
+class Hotels(Resource):
+
+    @marshal_with(resource_fields)
+    def get(self):
+        result = HotelModel.query.all()
+        if not result:
+            abort(404, message="Could not find hotel with that id")
+        return result
+
+
+class Hotel(Resource):
+
+    @marshal_with(resource_fields)
+    def get(self, hotel_id):
+        result = HotelModel.query.filter_by(id=hotel_id).first()
+        if not result:
+            abort(404, message="Could not find hotel with that id")
+        return result
+
+    @marshal_with(resource_fields)
+    def put(self, hotel_id):
+        args = hotel_put_args.parse_args()
+        result = HotelModel.query.filter_by(id=hotel_id).first()
+        if result:
+            abort(409, message="Hotel id taken")
+
+        hotel = HotelModel(id=hotel_id, name=args['name'], city=args['city'], address=args['address'])
+        db.session.add(hotel)
+        db.session.commit()
+        return hotel, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, hotel_id):
+        args = hotel_update_args.parse_args()
+        result = HotelModel.query.filter_by(id=hotel_id).first()
+        if not result:
+            abort(404, message="Hotel doesn't exist, cannot update")
+
+        if args['name']:
+            result.name = args['name']
+        if args['city']:
+            result.city = args['city']
+        if args['address']:
+            result.address = args['address']
+
+        db.session.commit()
+
+        return result, 200
+
+    @marshal_with(resource_fields)
+    def delete(self, hotel_id):
+        args = hotel_update_args.parse_args()
+        result = HotelModel.query.filter_by(id=hotel_id).first()
+        if not result:
+            abort(404, message="Hotel doesn't exist, cannot delete")
+
+        db.session.delete(result)
+        db.session.commit()
+
+        return "", 204
+
+
+api.add_resource(Hotel, "/api/hotels/<int:hotel_id>")
+api.add_resource(Hotels, "/api/hotels/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
